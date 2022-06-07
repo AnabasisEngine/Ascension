72961e32d47e5bfeb8576f2ec09fda6229ea1fb2
Khitiara <robotbrainify@gmail.com>
2022-06-06 23:28:19 -0400

    AA

--- Anabasis.Ascension/IAscensionScene.cs
+++ Anabasis.Ascension/IAscensionScene.cs
@@ -3,6 +3,10 @@ using Anabasis.Tasks;
 
 namespace Anabasis.Ascension;
 
+/// <summary>
+/// Provides an <see cref="IProgress{SceneLoadStatus}"/> overload for <see cref="IAnabasisContext.LoadAsync"/> to
+/// support easier loading screens.
+/// </summary>
 public interface IAscensionScene : IAnabasisContext
 {
     public AnabasisTask LoadAsync(IProgress<SceneLoadStatus> progress);
--- Anabasis.Ascension/SceneLoadStatus.cs
+++ Anabasis.Ascension/SceneLoadStatus.cs
@@ -1,4 +1,27 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace Anabasis.Ascension;
 
-public class SceneLoadStatus
-{ }
+public class SceneLoadStatus : INotifyPropertyChanged
+{
+    private float _progress;
+
+    public float Progress {
+        get => _progress;
+        set => SetField(ref _progress, value);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null) {
+        if (!EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
