90a3d1e51ede169defae3cec66fd17ad8fd34ef7
Khitiara <robotbrainify@gmail.com>
2022-06-07 13:53:44 -0400

    aa

--- Anabasis.Ascension/SceneLoadStatus.cs
+++ Anabasis.Ascension/SceneLoadStatus.cs
@@ -1,27 +1,6 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
 namespace Anabasis.Ascension;
 
-public class SceneLoadStatus : INotifyPropertyChanged
+public class SceneLoadStatus
 {
-    private float _progress;
-
-    public float Progress {
-        get => _progress;
-        set => SetField(ref _progress, value);
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null) {
-        if (!EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
-    }
+    public float Progress { get; set; }
 }
